2fb72ce0c925820aec8825f01ebc5ae338527676
adam.metal <adam.metal@codecool.com>
2023-05-16 15:29:37 +0200

    add new message feature

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,15 @@
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import { collection, query } from "firebase/firestore";
+import { collection, query, addDoc, serverTimestamp } from "firebase/firestore";
 import store from "../firebase/firestore";
 import BeerCard from "./BeerCard";
+import BeerMessage from "./BeerMessage";
 
 const beerConvert = {
   fromFirestore: (snap) => ({
     id: snap.id,
     ...snap.data(),
   }),
+  toFirestore: (doc) => doc
 };
 
 const beerRef = collection(store, "beer-feed").withConverter(beerConvert);
@@ -16,12 +18,20 @@ const beerQuery = query(beerRef);
 function Feed() {
   const [items, loading] = useCollectionData(beerQuery);
 
+  const handleSendMessage = async (message) => {
+    await addDoc(beerRef, {
+      ...message,
+      createdAt: serverTimestamp()
+    });
+  }
+
   if (loading === true) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="container d-flex flex-column gap-2">
+      <BeerMessage onSend={handleSendMessage} />
       {items.map((item) => (
         <BeerCard entry={item} key={item.id} />
       ))}
