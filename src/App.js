e7cac0de4da439e1d4dde2b73c07f62462197463
adam.metal <adam.metal@codecool.com>
2023-05-17 13:52:21 +0200

    add favicon and refactor navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import { signOut } from "firebase/auth";
-import auth from "./firebase/auth";
 import Login from "./components/Login";
 import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
 import { useAuth } from './context/AuthContext';
 
 function App() {
   const user = useAuth();
 
-  const handleLogout = () => {
-    signOut(auth);
-  };
-
   return (
     <div>
-      <nav className="navbar mb-4" style={{ backgroundColor: "#f7df1e" }}>
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Brewitter</span>
-          {user && (
-            <button className="btn btn-secondary" onClick={handleLogout}>
-              Logout
-            </button>
-          )}
-        </div>
-      </nav>
+      <Navbar />
       {user ? <Feed /> : <Login />}
     </div>
   );
