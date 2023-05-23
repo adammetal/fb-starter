c7d3f257b37efda8956c9f0b34616368526e8c86
adam.metal <adam.metal@codecool.com>
2023-05-23 13:08:16 +0200

    install material ui

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { CssBaseline } from "@mui/material";
 
 function Layout() {
   return (
     <>
+      <CssBaseline />
       <Navbar />
       <Outlet />
     </>
